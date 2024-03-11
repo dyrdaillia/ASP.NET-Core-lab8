53d6c4ebb71cb95138ae57679374be9fa4e32e3a
Illia Dyrda <106555644+dyrdaillia@users.noreply.github.com>
2024-03-11 11:35:02 +0100

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApplication8.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+}
---
+++ ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication8.Models;
+
+public class ProductController : Controller
+{ 
+    public ActionResult Index()
+    {
+        // Створення декількох прикладів продуктів (тестові дані)
+        var products = new List<Product>
+        {
+            new Product { ID = 1, Name = "Forza Horizon", Price = 60.99m, CreatedDate = DateTime.Now },
+            new Product { ID = 2, Name = "Gears Of War 5", Price = 35.50m, CreatedDate = DateTime.Now.AddDays(-1) },
+            new Product { ID = 3, Name = "Alan Wake", Price = 15.79m, CreatedDate = DateTime.Now.AddDays(-2) }
+        };
+
+        return View(products);
+    }
+}
